ed5df9b4bb98206958df312f88753e123f62be36
smaldera <maldera@to.infn.it>
2024-04-23 11:48:20 +0200

    adding run_reconTest.sh

---
+++ analysis_simo/run_reconTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# sf 1
+
+
+python  rec_Acdrecon.py -i sf_521262842_Ecut0_Max0.1.root  -ft2 /home/maldera/FERMI/code/plotAcdRates/data/sf/521262842/r0521262842_ft2.fit -o  provaInputPars.root --base_dir  /home/maldera/FERMI/code/plotAcdRates/data/sf/521262842/  -t 130 -t0 521262842  -sb 15 -k False -ts std
+
+
+
+
+
+
+
